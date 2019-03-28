fe93625f6eaf387dc6a4614bc15ccadcdf28e703
unknown <o.t.wathsala@gmail.com>
2019-03-28 09:08:50 +0530

    two way binding anabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 //import { ModelsComponent } from './models/models.component';
@@ -12,7 +12,8 @@ import {UserServices} from './Services/User.services';
     UserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [UserServices],
   bootstrap: [AppComponent]
