0b9045eb8a60695dd3f50cbb8e3d8d4ef6a1e191
unknown <o.t.wathsala@gmail.com>
2019-03-28 11:21:56 +0530

    first phase finished

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { User } from '../models/User';
 import { UserServices } from '../Services/User.services';
 
@@ -12,14 +12,27 @@ export class UserComponent implements OnInit {
     user:User // user variable and User is type
 
   isPersonDetailsEditableDisabled=true; // used to control disable property in personal details
-
+  isContactDetailsEditableDisabled=true;// control disable property in contact details
+  //@ViewChild("contactListt") contactList:ElementRef; // to refer each element
   
 
 
   constructor(private userService:UserServices) { }
 
-  PersonDetailsEditButtonClick(event){
+  PersonDetailsEditButtonClick(event){     //used to edit person details
     return this.isPersonDetailsEditableDisabled=false;
+  }
+
+  ContactDetailsEditableDisabled(event){    //used to edit contact detail
+    return this.isContactDetailsEditableDisabled=false;
+
+  }
+
+  addNewContact(event){
+     const addNewContact= prompt("Enter number");
+     this.user.contacts.push(addNewContact);
+
+
   }
 
   ngOnInit() {
