0402f32b8000e3e845d76b1904b8086e16960572
unknown <o.t.wathsala@gmail.com>
2019-03-28 09:02:26 +0530

    edit button enable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,13 +11,19 @@ export class UserComponent implements OnInit {
 
     user:User // user variable and User is type
 
-  isPersonDetailsEditableDisabled=true;
+  isPersonDetailsEditableDisabled=true; // used to control disable property in personal details
+
   
+
 
   constructor(private userService:UserServices) { }
 
+  PersonDetailsEditButtonClick(event){
+    return this.isPersonDetailsEditableDisabled=false;
+  }
+
   ngOnInit() {
-    this.user= this.userService.getUser("1");
+    this.user= this.userService.getUser("1"); 
   }
 
 }
